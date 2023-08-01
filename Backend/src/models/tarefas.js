1753610f2df6b41023524f8b2914b332c0c0584d
CaioSC01 <caio180g@gmail.com>
2023-08-01 00:06:15 -0300

    utilizando a dbconnecton

--- Backend/src/models/tarefas.js
+++ Backend/src/models/tarefas.js
@@ -1,5 +1,10 @@
-const listarTarefas = () => {};
+const dbConnection = require("../config/dbConnect.js");
+
+const listarTarefas = async () => {
+    const tarefas = await dbConnection.execute("SELECT * FROM tarefas");
+    return tarefas;
+};
 
 module.exports = {
-	listarTarefas,
+    listarTarefas,
 };
