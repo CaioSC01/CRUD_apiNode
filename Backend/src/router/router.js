10a11cccba8a28ebf00256146f93601feb5dc52b
CaioSC01 <caio180g@gmail.com>
2023-08-01 00:06:31 -0300

    mudando a router

--- Backend/src/router/router.js
+++ Backend/src/router/router.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const tarefasController = require("../controllers/tarefasController.js");
 
 const router = express.Router();
+
+router.get("/tarefas", tarefasController.listarTarefas);
 
 module.exports = router;
